2cbedb353012d3bdd6aa59390b046ecb7cd8a356
Fathiyam99 <fathiyamusse8@gmail.com>
2023-09-19 12:55:36 -0500

    changed nav color and names

--- ghi/app/src/Nav.js
+++ ghi/app/src/Nav.js
@@ -3,9 +3,9 @@ import { Link } from 'react-router-dom';
 
 function Nav() {
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-success">
+    <nav className="navbar navbar-expand-lg navbar-dark bg-info">
       <div className="container-fluid">
-        <Link className="navbar-brand" to="/">CarCar</Link>
+        <Link className="navbar-brand" to="/">DrivenCare</Link>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
